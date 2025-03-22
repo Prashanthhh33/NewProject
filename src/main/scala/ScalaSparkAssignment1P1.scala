0be4065602cd076da633e29d5e181b9e201d3ed5
Prasanthhh33 <prashanthinjamoor@gmail.com>
2025-03-23 02:45:01 +0530

    made changes

--- src/main/scala/ScalaSparkAssignment1P1.scala
+++ src/main/scala/ScalaSparkAssignment1P1.scala
@@ -1,10 +1,11 @@
 import org.apache.spark.SparkContext
 object ScalaSparkAssignment1P1 {
   def main(args: Array[String]): Unit = {
-    val sc = new SparkContext("local[*]","BigData")
-    val data = Array(10,20,30,40,50,60,70,80,90,100)
-    val rdd = sc.parallelize(data)
+    val sc = new SparkContext("local[2]","BigData")
+    val data = List(10,20,30,40,50,60,70,80,90,100)
+    val rdd = sc.parallelize(data,1)
     rdd.collect().foreach(println)
+    scala.io.StdIn.readLine()
   }
 
 }
