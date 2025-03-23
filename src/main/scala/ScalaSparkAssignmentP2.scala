fd99b476aaafafaf76ef685c6d1a4f3eb7e02c6e
Prasanthhh33 <prashanthinjamoor@gmail.com>
2025-03-23 15:00:09 +0530

    assignment1p2

--- src/main/scala/ScalaSparkAssignmentP2.scala
+++ src/main/scala/ScalaSparkAssignmentP2.scala
@@ -1,6 +1,10 @@
 import org.apache.spark.SparkContext
 object ScalaSparkAssignmentP2 {
   def main(args: Array[String]):Unit = {
+    val sc = new SparkContext("local[*]","SaveFile")
+    val data = Array(1,2,3,4,5,6,7,8,9,10)
+    val rdd = sc.parallelize(data)
+    rdd.saveAsTextFile("C:/Users/prash/OneDrive/Desktop/samplefile.txt")
 
   }
 
