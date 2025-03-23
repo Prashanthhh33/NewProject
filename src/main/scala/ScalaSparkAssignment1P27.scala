d465985e1f268e1ae6e3969a9b5cfe76a62632ba
Prasanthhh33 <prashanthinjamoor@gmail.com>
2025-03-24 01:35:12 +0530

    spark assignments

--- src/main/scala/ScalaSparkAssignment1P27.scala
+++ src/main/scala/ScalaSparkAssignment1P27.scala
@@ -6,8 +6,8 @@ object ScalaSparkAssignment1P27 {
     val rdd = sc.parallelize(data)
     val rdd1 = rdd.flatMap(x=>x.split(" "))
     val rdd2 = rdd1.map(x=>(x,1))
-    val rdd3 = rdd2.reduceByKey((x,y)=>(x+y))
-    val rdd4 = rdd3.sortBy(x=>x._2,false)
+    val rdd3 = rdd2.reduceByKey((x,y)=>x+y)
+    val rdd4 = rdd3.sortBy(x=>x._2, ascending = false)
     rdd4.take(5).foreach(println)
 
   }
