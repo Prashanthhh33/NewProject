e3c89bdfb3716872761fa8b885f58d7b83faeb32
Prasanthhh33 <prashanthinjamoor@gmail.com>
2025-03-23 23:31:54 +0530

    assignment1p3

--- src/main/scala/ScalaSparkAssignment1P3.scala
+++ src/main/scala/ScalaSparkAssignment1P3.scala
@@ -5,9 +5,9 @@ object ScalaSparkAssignment1P3 {
     val data = Array(5,10,15,20,25)
     val rdd= sc.parallelize(data)
     val with_mean = rdd.mean()
-    val rdd1 = rdd.sum()
-    val rdd2 = rdd.count()
-    val without_mean = rdd1/rdd2
+    val rdd_sum = rdd.reduce(_+_)
+    val rdd1 = rdd.count()
+    val without_mean = rdd_sum/rdd1.toDouble
     println(with_mean)
     println(without_mean)
   }
