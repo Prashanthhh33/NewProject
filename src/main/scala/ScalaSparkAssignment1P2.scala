dd96d79d4945440e27a4ed53d889582fbef3333d
Prasanthhh33 <prashanthinjamoor@gmail.com>
2025-03-23 15:02:07 +0530

    assignment1p2

--- src/main/scala/ScalaSparkAssignment1P2.scala
+++ src/main/scala/ScalaSparkAssignment1P2.scala
@@ -1,5 +1,5 @@
 import org.apache.spark.SparkContext
-object ScalaSparkAssignmentP2 {
+object ScalaSparkAssignment1P2 {
   def main(args: Array[String]):Unit = {
     val sc = new SparkContext("local[*]","SaveFile")
     val data = Array(1,2,3,4,5,6,7,8,9,10)
